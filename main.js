b3d7d101970bcb74f87603c6a3ec6c623e2f75e7
Ardi <ardiantosaputro2001@gmail.com>
2024-11-22 00:08:24 +0700

    js dengan jQuery, untuk merubah css class select priority sudah selesai

--- main.js
+++ main.js
@@ -7,3 +7,35 @@ buttons.forEach((element) => {
 	});
 });
 
+// priority = document.querySelectorAll(".select-priority");
+// priority.forEach((element) => {
+// 	if (element.value == "low") {
+// 		element.style.backgroundColor = "red";
+// 	}
+
+// 	if (element.value == "medium") {
+// 		element.style.backgroundColor = "blue";
+// 	}
+// });
+
+$(document).ready(function () {
+	$(".select-priority").each(function () {
+		if ($(this).val() == "low") {
+			$(this).addClass("low-priority");
+		}
+
+		if ($(this).val() == "medium") {
+			$(this).addClass("medium-priority");
+		}
+
+		if ($(this).val() == "high") {
+			$(this).addClass("high-priority");
+		}
+
+		$(this).on("change", function () {
+			let className = $(this).attr("class");
+			$(this).removeClass(className.slice("16"));
+			$(this).addClass($(this).val() + "-priority");
+		});
+	});
+});
