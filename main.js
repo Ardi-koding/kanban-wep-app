07079e8f65e34a4a54d326b1ba288e39dd239cf6
Ardi <ardiantosaputro2001@gmail.com>
2024-11-29 03:04:56 +0700

    add task functionality, perlu dirapikan, aku asal nulis aja karena ngantuk

--- main.js
+++ main.js
@@ -22,7 +22,37 @@ $(document).ready(function () {
 	}
 	checkPriority();
 
-	function createTask(val) {
+	function whichSection(val, dataLength, newTask) {
+		switch (val) {
+			case "to-do":
+				let attributeT = val + "-" + dataLength;
+				console.log(attributeT);
+				let attributeT2 = val + "-" + (dataLength + 1);
+				newTask.attr("data-list", attributeT2);
+				let selector = `[data-list="${attributeT}"]`;
+				$(selector).after(newTask);
+				break;
+			case "in-progress":
+				let attributeI = val + "-" + (dataLength + 1);
+				newTask.attr("data-list", attributeI);
+				$(attributeI).after(newTask);
+				break;
+			case "review":
+				let attributeR = val + "-" + (dataLength + 1);
+				newTask.attr("data-list", attributeR);
+				$(attributeR).after(newTask);
+				break;
+			case "done":
+				let attributeD = val + "-" + (dataLength + 1);
+				newTask.attr("data-list", attributeD);
+				$(attributeD).after(newTask);
+				break;
+			default:
+				break;
+		}
+	}
+
+	function createTask(whichSection, namaClass, dataListElement) {
 		let taskList = $("<div></div>");
 		taskList.addClass("task_list");
 
@@ -56,11 +86,14 @@ $(document).ready(function () {
 
 		taskList.append(priority, inlineInput, buttonGroup);
 
-		$(".to-do_1").after(taskList);
+		whichSection(namaClass, dataListElement, taskList);
 		checkPriority();
 	}
 
-	$(".add-button").on("click", function () {
-		createTask($(this).val());
+	$(".add-task").on("click", function () {
+		let namaClass = $(this).parent().attr("class").slice("5");
+		let parent = $(this).parent();
+		let dataListElement = Number(parent.children("[data-list]").length);
+		createTask(whichSection, namaClass, dataListElement);
 	});
 });
